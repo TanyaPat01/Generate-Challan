74ffe2707620cb360395a094e742fef73736bbb9
Adya Tiwari <103478795+adya14@users.noreply.github.com>
2023-12-08 21:24:23 +0530

    Create test _app.py

---
+++ test _app.py
@@ -0,0 +1,17 @@
+# test_my_code.py
+
+def add(x, y):
+    return x + y
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+def subtract(x, y):
+    return x - y
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(1, 1) == 0
+    assert subtract(0, 0) == 0
